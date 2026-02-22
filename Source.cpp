4bd2c34ecf975d572f75c1d00b88599c5f737423
Dustin Carpenter <88062861+Subzero5401@users.noreply.github.com>
2026-02-22 07:25:52 -0600

    Implement safe memory deletion for IntegerParsed
    Added memory deallocation for IntegerParsed variable.

--- Source.cpp
+++ Source.cpp
@@ -8,4 +8,5 @@ int main() {
 	char* IntegerParsed;
 	IntegerParsed = Parseinteger(12345678906464);
 	std::cout << IntegerParsed;
+	SafeDelete(IntegerParsed); // Safely deletes the allocated memory from called function. 
 }
